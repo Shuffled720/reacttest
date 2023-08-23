3b8b3d243a709561822be2913edfc38014ae795f
Shuffled72 <shuffled720@gmail.com>
2023-08-23 12:04:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Photo from './comps/Photo';
+function App() {
+  return (
+    <>
+      <div className='grid'>
+        <Photo />
+        <Photo />
+        <Photo />
+        <Photo />
+        <Photo />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/comps/Photo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Photo = () => {
+    return (
+        <>
+            <img src="https://picsum.photos/200/300" alt="random" />
+        </>
+    )
+}
+
+export default Photo
